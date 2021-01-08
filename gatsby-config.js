f661c68d9a12bf5de2c8996aafbe1abbb53e22db
Jordan Koffman <jkoffman@g.harvard.edu>
2021-01-08 11:28:57 -0500

    add purpose and models

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,8 @@ module.exports = {
           },
         ],
       },
-
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -44,8 +45,5 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
